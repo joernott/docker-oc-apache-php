4920b097096877706e4b5a0e73fc2f1ab073c887
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 22:25:47 +0100

    Fix label, add image history support

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
+set -x
+curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
+source src/tmp/install/functions.sh
 
+patch_dockerfile Dockerfile56
 docker build -f Dockerfile56 -t registry.ott-consult.de/oc/apache-php:5.6 .
 docker push registry.ott-consult.de/oc/apache-php:5.6
+
+patch_dockerfile Dockerfile70
 docker build -f Dockerfile70 -t registry.ott-consult.de/oc/apache-php:7.0 .
 docker push registry.ott-consult.de/oc/apache-php:7.0
+
+patch_dockerfile Dockerfile71
 docker build -f Dockerfile71 -t registry.ott-consult.de/oc/apache-php:7.1 .
 docker tag registry.ott-consult.de/oc/apache-php:7.1 registry.ott-consult.de/oc/apache-php:latest
 docker push registry.ott-consult.de/oc/apache-php:7.1
 docker push registry.ott-consult.de/oc/apache-php:latest
-
