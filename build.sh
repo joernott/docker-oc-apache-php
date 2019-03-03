b583728859a709450b6a40ff4a3008b41b295b94
Joern Ott <joern.ott@ott-consult.de>
2019-03-03 20:37:20 +0000

    Remove php 5.6

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -x
 curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source src/tmp/install/functions.sh
 
-patch_dockerfile Dockerfile56
-docker build -f Dockerfile56 -t registry.ott-consult.de/oc/apache-php:5.6 .
-docker push registry.ott-consult.de/oc/apache-php:5.6
-
 patch_dockerfile Dockerfile72
 docker build -f Dockerfile72 -t registry.ott-consult.de/oc/apache-php:7.2 .
 docker tag registry.ott-consult.de/oc/apache-php:7.2 registry.ott-consult.de/oc/apache-php:latest
